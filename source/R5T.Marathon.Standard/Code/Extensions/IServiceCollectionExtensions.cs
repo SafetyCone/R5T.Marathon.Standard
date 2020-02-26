a42c656e78224d7d36d9c92bf83419c900fcdb0f
MinexAutomation <minex.automation@gmail.com>
2020-02-26 09:49:50 -0800

    Added IServiceCollection extensions.

---
+++ source/R5T.Marathon.Standard/Code/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Dacia;
+
+using R5T.Marathon.Default;
+
+
+namespace R5T.Marathon.Standard
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the <see cref="BackgroundWorkItemQueueProcessor"/> as a <see cref="Microsoft.Extensions.Hosting.IHostedService"/>.
+        /// </summary>
+        /// <remarks>
+        /// No corresponding AddXAction() method for this service since the queue processor will never be injected.
+        /// </remarks>
+        public static IServiceCollection AddBackgroundWorkItemQueueProcessor(this IServiceCollection services,
+            ServiceAction<IBackgroundWorkItemQueue> addBackgroundWorkItemQueue)
+        {
+            services
+                .AddHostedService<BackgroundWorkItemQueueProcessor>()
+                .RunServiceAction(addBackgroundWorkItemQueue)
+                ;
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="BackgroundWorkItemQueueProcessor"/> as a <see cref="Microsoft.Extensions.Hosting.IHostedService"/> using the <see cref="BackgroundWorkItemQueue"/> implementation of <see cref="IBackgroundWorkItemQueue"/>.
+        /// </summary>
+        public static IServiceCollection AddBackgroundWorkItemQueueProcessor(this IServiceCollection services)
+        {
+            services.AddBackgroundWorkItemQueueProcessor(services.AddBackgroundWorkItemQueueAction());
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="BackgroundWorkItemQueueProcessor"/> as a <see cref="Microsoft.Extensions.Hosting.IHostedService"/> using the <see cref="BackgroundWorkItemQueue"/> implementation of <see cref="IBackgroundWorkItemQueue"/>.
+        /// </summary>
+        public static ServiceAction<IBackgroundWorkItemQueue> AddBackgroundWorkItemQueueAndProcessorAction(this IServiceCollection services)
+        {
+            var serviceAction = new ServiceAction<IBackgroundWorkItemQueue>(() => services.AddBackgroundWorkItemQueueProcessor());
+            return serviceAction;
+        }
+    }
+}
