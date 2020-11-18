f1bb031a7ab05ecb814e875adb43ac2568172833
David Coats <david@getsafetycone.com>
2020-11-17 22:58:31 -0800

    Add IExceptionSink service dependency

--- source/R5T.Marathon.Standard/Code/Extensions/IServiceCollectionExtensions.cs
+++ source/R5T.Marathon.Standard/Code/Extensions/IServiceCollectionExtensions.cs
@@ -2,6 +2,8 @@
 
 using Microsoft.Extensions.DependencyInjection;
 
+using R5T.D0049;
+
 using R5T.Dacia;
 
 using R5T.Marathon.Default;
@@ -18,11 +20,13 @@ namespace R5T.Marathon.Standard
         /// No corresponding AddXAction() method for this service since the queue processor will never be injected.
         /// </remarks>
         public static IServiceCollection AddBackgroundWorkItemQueueProcessor(this IServiceCollection services,
-            IServiceAction<IBackgroundWorkItemQueue> addBackgroundWorkItemQueue)
+            IServiceAction<IBackgroundWorkItemQueue> addBackgroundWorkItemQueue,
+            IServiceAction<IExceptionSink> exceptionSinkAction)
         {
             services
                 .AddHostedService<BackgroundWorkItemQueueProcessor>()
                 .RunServiceAction(addBackgroundWorkItemQueue)
+                .Run(exceptionSinkAction)
                 ;
 
             return services;
@@ -31,11 +35,14 @@ namespace R5T.Marathon.Standard
         /// <summary>
         /// Adds the <see cref="BackgroundWorkItemQueueProcessor"/> as a <see cref="Microsoft.Extensions.Hosting.IHostedService"/> using the <see cref="BackgroundWorkItemQueue"/> implementation of <see cref="IBackgroundWorkItemQueue"/>.
         /// </summary>
-        public static IServiceCollection AddBackgroundWorkItemQueueProcessor(this IServiceCollection services)
+        public static IServiceCollection AddBackgroundWorkItemQueueProcessor(this IServiceCollection services,
+            IServiceAction<IExceptionSink> exceptionSinkAction)
         {
             var backgroundWorkItemQueueAction = services.AddBackgroundWorkItemQueueAction();
 
-            services.AddBackgroundWorkItemQueueProcessor(backgroundWorkItemQueueAction);
+            services.AddBackgroundWorkItemQueueProcessor(
+                backgroundWorkItemQueueAction,
+                exceptionSinkAction);
 
             return services;
         }
